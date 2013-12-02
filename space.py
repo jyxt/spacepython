3a10ea7352eaa777bbaeced079d193ec966750e0
Sheldon <chi.sheldon@gmail.com>
2013-12-02 02:09:46 -0500

    added some order operations

--- space.py
+++ space.py
@@ -4,12 +4,17 @@ import json
 import re
 import copy
 
-splitInputStringPattern = re.compile('\n(?! )')  # newline with no space
-spacePattern = re.compile('^([^ ]+)(\n|$)')  # not space + newline|eol
-leafPattern = re.compile('^([^ ]+) ')  # not space + one space
+splitInputStringPattern = re.compile(r'\n(?! )')  # newline with no space
+spacePattern = re.compile(r'^([^ ]+)(\n|$)')  # not space + newline|eol
+leafPattern = re.compile(r'^([^ ]+) ')  # not space + one space
 
 
 class Space:
+
+    """
+    Space is key : value pair where key is string and
+    value is either string or another Space object
+    """
 
     def __init__(self, content=None):
         self.__data = []
@@ -21,6 +26,8 @@ class Space:
             self.__load_from_string(content)
         elif isinstance(content, Space):
             self.__data = copy.deepcopy(content._Space__data)  # not the best way to do it
+        else:
+            raise NotImplementedError('currently can only instantiate from string and Space object')
 
     def __load_from_string(self, string):
 
@@ -44,23 +51,26 @@ class Space:
                 match = leafPattern.search(space).groups()[0]
                 self.__set_data(match, space[len(match)+1:].replace('\n ', '\n'))
 
-    def __set_data(self, key, value):
-        # if exist then replace otherwise append
-        if self.has(key):
+    def __set_data(self, key, value, index=None, overwrite=False):
+        if index is None:  # can't use if not index here cuz python eval 0 to False
+            self.__data.append((key, value))
+        elif overwrite:
             self.__data[self.index_of(key)] = (key, value)
         else:
-            self.__data.append((key, value))
+            self.__data.insert(index, (key, value))
 
-    def append(self, key, value):
-        self.__data.append((key, value))
 
     def __get_value_by_key(self, key):
-        # [v for k, v in self.__data if k == key]  # returns a list values with matching key
         return next((v for k, v in self.__data if k == key), None)
 
     def __get_value_by_string(self, string):
         if not string:
             raise ValueError("input string can't be empty")
+
+        # added check if xpath for earlier return
+        if not Space.is_xpath(string):
+            return self.__get_value_by_key(string)
+
         string = string.strip()  # remove leading and trailing whitespace
 
         # single entry, return from __data[key]
@@ -78,35 +88,59 @@ class Space:
         if firstValue:
             return firstValue.__get_value_by_string(rest)
 
-        # TODO: verify get, set input types
     def get(self, content):
+        content = str(content)
         return self.__get_value_by_string(content)
 
-    def set(self, path, value):
-        if path and value:
-            path = str(path)
-            return self.__set_value_by_path(path, value)
+    def set(self, key, value, index=None):
+        key = str(key)
+        if Space.is_xpath(key):
+            self.__set_value_by_path(key, value)
+        elif self.has(key):
+            print self.index_of(key)
+            self.__set_data(key, value, index=self.index_of(key), overwrite=True)
+        else:
+            self.__set_data(key, value, index=index)
+        # TODO: add trigger
+        return self
 
-    def __set_value_by_path(self, path, value):
-        if " " not in path:
-            self.__set_data(path, value)
-            return self  # for chanining
-        path = ' '.join(path.split())
-        first, separator, rest = path.partition(' ')
-        return self.__get_value_by_key(first).__set_value_by_path(rest, value)
+    def __set_value_by_path(self, path, value):  # called setByXPath in js version
+        if path is None:
+            return None  # return null in js version
+        keys = str(path).split(' ')
+        context = self
+        isLeaf = lambda index: index == len(keys) - 1
+        for index, key in enumerate(keys):
 
-    # TODO:
-    def is_xpath(self, content):
-        return
+            if not isLeaf and isinstance(context.__get_value_by_key(key), Space):
+                context = context.get(key)
+                continue
+
+            if index == (len(keys)-1):
+                new_val = value
+            else:
+                new_val = Space()
+
+            if context.has(key):
+                context.__set_data(key, new_val, index=context.index_of(key), overwrite=True)
+            else:
+                context.__set_data(key, new_val)
+            context = context.get(key)
+        return self
+
+
+    @staticmethod
+    def is_xpath(content):
+        return ' ' in content
 
     # misc
 
     def has(self, key):
-        if self.__data:
-            return key in zip(*self.__data)[0]
+        return self.__get_value_by_key(key) is not None
 
     def __clear(self):
         self.__data = []
+        return self
 
     def clear(self, string=None):
         self.__clear()
@@ -124,8 +158,7 @@ class Space:
         return zip(*self.__data)[0]
 
     def index_of(self, key=None):
-        if key and self.has(key):
-            return self.get_keys().index(key)
+        return self.get_keys().index(key)
 
     # JSON
     def jsonable(self):
@@ -154,8 +187,6 @@ class Space:
             string += ' '*spaces + key  # equivalent of Space.strRpeat in js code
             if isinstance(value, Space):
                 string += '\n' + value.__str_helper(spaces + 1)
-    #        elif value == '':
-     #           string += ' \n'
             elif '\n' in value.__str__():
                 string += ' ' + value.replace('\n', ('\n' + ' '*(spaces + 1))) + '\n'
             else:
@@ -163,16 +194,42 @@ class Space:
 
         return string
 
+    # ordered operations
+    def each(self, fn):
+        """
+        fn should take one argument (k,v),
+        different from js version
+        """
+        return map(fn, self.__data)
+
+    def append(self, key, value):
+        self.__data.append((key, value))
+
+    def prepend(self, key, value):
+        self.__data.insert(0, (key, value))
+
+    def concat(self, b):
+        if isinstance(b, basestring):
+            b = Space(b)
+        self.__data.extend(b._Space__data)
+        return self
+
+    def create(self, k, v):
+        self.__data.append([k, v])
+
+    # observer pattern with on, off, trigger
+
+
+
 
 if __name__ == '__main__':
-    space = Space('name John\n age\nfavoriteColors\n blue\n green\n red 1\n')
+    space = Space('name John\nage\nfavoriteColors\n blue\n  blue1 1\n  blue2 2\n green\n red 1\n')
+
+
     print space.to_json()
 
     print space
-
-
-
-
+    print space.set('favoriteColors blue', 'aaa')
 
 
 
--- test_space.py
+++ test_space.py
@@ -6,41 +6,41 @@ __author__ = 'tesse'
 
 class TestSpace(TestCase):
 
-    def testSpaceExist(self):
+    def test_space_exist(self):
         self.assertTrue(isinstance(Space(), Space), 'Space should exist')
 
-    def testIndexOf(self):
+    def test_index_of(self):
         space = Space("Hello World")
         self.assertEqual(space.length(), 1)
         self.assertEqual(space.index_of('Hello'), 0, 'indexOf should be correct')
 
-    def testGetSet(self):
+    def test_get_set(self):
         space = Space('Hello World')
         self.assertEqual(space.get("Hello"), 'World', 'Property should be accessible')
         self.assertEqual(type(space.get("Hello")), str, 'Leaf should be string')
         space.set('foo', 'bar')
         self.assertEqual(space.get('foo'), 'bar', 'Space should be modifiable' )
 
-    def testNestSpace(self):
+    def test_nest_space(self):
         space = Space('foobar\n one 1')
         self.assertTrue(isinstance(space.get('foobar'), Space), 'Nested space should be Space type')
 
-    def testKeyNotExist(self):
+    def test_key_not_exist(self):
         space = Space('foobar\n leaf 1')
         self.assertEqual(space.get('Hi'), None, 'Should return None if key does not exist')
 
-    def testPathNotExist(self):
+    def test_path_not_exist(self):
         space = Space('Hello\n one 1\n two 2')
         self.assertEqual(space.get('Hello world one'), None, 'Should return None if path does not exist')
 
-    def testLength(self):
+    def test_length(self):
         space = Space('list\nsingle value')
         self.assertEqual(space.length(), 2, 'space should have 2 names')  # 'name' here means 'key'
         self.assertTrue(isinstance(space.get('list'), Space), 'name without a trailing space should be name')
 
         # skipped a couple of tests here,  don't think can create from object literal in python
 
-    def testMultiLine(self):
+    def test_multi_line(self):
         string = 'user\n\
 name Aristotle\n\
 admin false\n\
@@ -65,9 +65,19 @@ domains\n\
         space = Space(string)
         self.assertEqual(space.get('domains test.test.com pages home settings data title'), 'Hello, World', 'Multiline creation shuold be OK')
 
+    def test_each(self):
+        space = Space('hello world\nhi mom')
+        string = ''
+        self.assertEqual(space.each(lambda (k, v): k), ['hello', 'hi'])
+        
+    def test_prepend(self):
+        space = Space('hello world')
+        space.prepend('first', 'word')
+        self.assertEqual(space.__str__(), 'first word\nhello world\n')
+        
 
 class TestAppend(TestCase):  # haven't done the 'on append incre count' thing as in js verion
-    def testAppend(self):
+    def test_append(self):
         space = Space('hello world')
         space.append('foo', 'bar')
         space.set('foo2', 'bar')
@@ -76,16 +86,14 @@ class TestAppend(TestCase):  # haven't done the 'on append incre count' thing as
         self.assertEqual(space.length(), 4)
 
 
-
-
 class TestClear(TestCase):
-    def testClear(self):
+    def test_clear(self):
         space = Space('Hello world')
         self.assertEqual(space.length(), 1)
         self.assertTrue(isinstance(space.clear(), Space), 'clear returns Space so chainable')
         self.assertEqual(space.length(), 0)
         
-    def testClearWithSpaceArgument(self):
+    def test_clear_with_space_argument(self):
         space = Space("Hellow world")
         space.clear("hey there")
         self.assertEqual(space.length(), 1)
@@ -106,8 +114,16 @@ class TestClone(TestCase):
         self.assertEqual(space3.get('anotherSpace 123'), '456')
         
 
+class TestConcat(TestCase):
+    def testConcat(self):
+        space1 = Space('hi world')
+        b = Space('hello there')
+        space1.concat(b)
+        self.assertEqual(space1.get('hello'), 'there')
+        
+
 class TestMultiline(TestCase):
-    def testMultiline(self):
+    def test_multiline(self):
         space = Space('my multiline\n string')
         self.assertEqual(space.get('my'), 'multiline\nstring')
         
@@ -121,8 +137,6 @@ class TestMultiline(TestCase):
         space4 = Space('brave \n new\n world')
         self.assertEqual(space4.get('brave'), '\nnew\nworld', 'ml begin with nl value correct')
         
-        space5 =  Space('brave \n new\n world')
-
 
 class TestToStr(TestCase):
     def test__str__(self):
@@ -145,3 +159,29 @@ class TestToStr(TestCase):
         space2.set('other', 'foobar')
         self.assertEqual(space2.__str__(), 'john\n age 5\nmultiline hello\n world\nother foobar\n')
 
+
+class TestSet(TestCase):
+    def test_set(self):
+        space = Space('hello world')
+        self.assertEqual(space.get('hello'), 'world')
+        self.assertTrue(isinstance(space.set('hello', 'mom'), Space), 'set returns self for chaining')
+        
+        intSpace = Space()
+        intSpace.set(2, 'hi')
+        self.assertEqual(intSpace.get(2), 'hi')
+        
+        emptyStringSpace = Space()
+        emptyStringSpace.set('boom', '')
+        self.assertEqual(emptyStringSpace.get('boom'), '')
+        
+        # this is important, value should have been changed
+        self.assertEqual(space.get('hello'), 'mom')
+        space.set('head style color', 'blue')
+        self.assertEqual(space.get('head style color'), 'blue')
+
+
+class TestHas(TestCase):
+    def test_has(self):
+        space = Space('hello world')
+        self.assertTrue(space.has('hello'))
+        self.assertFalse(space.has('world'))
